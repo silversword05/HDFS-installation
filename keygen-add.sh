3ed6b7469f39ad99cbc8ce66aa8fb99a8ec77c37
Aadi Swadipto Mondal <silverswordaadi05@gmail.com>
2022-12-14 13:20:36 -0600

    Update keygen-add.sh

--- keygen-add.sh
+++ keygen-add.sh
@@ -2,9 +2,9 @@
 trap "exit" INT
 
 # Fill configurations here
-MASTER=(22 amondal5@c220g2-011320.wisc.cloudlab.us)
-CHILD1=(22 amondal5@c220g2-011319.wisc.cloudlab.us)
-CHILD2=(22 amondal5@c220g2-011315.wisc.cloudlab.us)
+MASTER=(<master-port> <username>@<master-machine-name>)
+CHILD1=(<child1-port> <username>@<child1-machine-name>)
+CHILD2=(<child2-port> <username>@<child2-machine-name>)
 
 # Generate master key
 MASTER_COMMAND="ssh-keygen -t rsa -q -N '' <<< $'\ny'; echo '\n'; cat ~/.ssh/id_rsa.pub;"
